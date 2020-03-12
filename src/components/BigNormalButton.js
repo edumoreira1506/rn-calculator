977ecc7746419069877742df8eb5235017263ce0
Eduardo Moreira <contato@eduardoem.com.br>
2020-03-12 19:25:13 -0300

    Add buttons to calculator

---
+++ src/components/BigNormalButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TouchableHighlight, StyleSheet, Text } from 'react-native';
+
+const BigNormalButton = ({
+  onClick,
+  text,
+}) => (
+  <TouchableHighlight
+    onPress={onClick}
+    style={styles.buttonContainer}
+  >
+    <Text style={styles.buttonText}>
+      { text }
+    </Text>
+  </TouchableHighlight>
+);
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    display: 'flex',
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#dee1e1',
+    height: '20%',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderColor: '#192a38'
+  },
+  buttonText: {
+    fontSize: 30,
+    color: '#192a38',
+  }
+});
+
+export default BigNormalButton;
--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,14 +1,97 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Display from '../components/Display';
+import NormalButton from '../components/NormalButton';
+import SecondaryButton from '../components/SecondaryButton';
+import BigNormalButton from '../components/BigNormalButton';
 
 const Calculator = () => {
   const [ content, setContent ] = useState('1 + 1 = 2');
+
+  const exampleOnClick = () => console.log('clicked!');
 
   return (
     <View style={styles.calculatorContainer}>
       <View style={styles.displayContainer}>
         <Display content={content} />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <NormalButton
+          onClick={exampleOnClick}
+          text="AC"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="#"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="#"
+        />
+        <SecondaryButton
+          onClick={exampleOnClick}
+          text="/"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="7"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="8"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="9"
+        />
+        <SecondaryButton
+          onClick={exampleOnClick}
+          text="*"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="4"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="5"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="6"
+        />
+        <SecondaryButton
+          onClick={exampleOnClick}
+          text="-"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="1"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="2"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text="3"
+        />
+        <SecondaryButton
+          onClick={exampleOnClick}
+          text="+"
+        />
+        <BigNormalButton
+          onClick={exampleOnClick}
+          text="0"
+        />
+        <NormalButton
+          onClick={exampleOnClick}
+          text=","
+        />
+        <SecondaryButton
+          onClick={exampleOnClick}
+          text="="
+        />
       </View>
     </View>
   );
@@ -25,6 +108,13 @@ const styles = StyleSheet.create({
     display: 'flex',
     width: '100%',
     height: '25%',
+  },
+  buttonsContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    height: '75%',
+    width: '100%',
+    flexDirection: 'row'
   }
 });
 
